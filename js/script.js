f5fcdc36a376e9fa380b9d3d8c74a6e9fd7f1b7f
Jordan Patel <21124677+JordansGit@users.noreply.github.com>
2023-04-11 17:32:30 +0100

    created and styled mobile nav

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ let featuresNav = document.querySelector('.nav-features-list');
 let companyNav = document.querySelector('.nav-company-list');
 let featuresArrow = document.querySelector('.nav-item-features > .arrow-icon')
 let companyArrow = document.querySelector('.nav-item-company > .arrow-icon')
-
-console.log(featuresArrow.src);
+let mobileMenu = document.querySelector('.mobile-menu');
+let mobileNav = document.querySelector('.nav-container');
+let mobileCloseBtn = document.querySelector('.nav-close-btn');
 
 featuresItem.addEventListener('click', function() {
   if (featuresNav.style.display === 'block') {
@@ -26,3 +27,23 @@ companyItem.addEventListener('click', function() {
     companyArrow.src = './images/icon-arrow-up.svg';
   }
 })
+
+mobileMenu.addEventListener('click', function() {
+  mobileNav.classList.add('nav-open-nav');
+  document.body.classList.add('nav-open-body');
+})
+
+mobileCloseBtn.addEventListener('click', function() {
+  mobileNav.classList.remove('nav-open-nav');
+  document.body.classList.remove('nav-open-body');
+})
+
+/* 
+Learning notes: 
+  It's probably better if I do the classlist add/remove/toggle for the nested nav 
+  like i've done with mobile menu & mobile close btn, 
+  instead of adding and removing styles in js. 
+
+  This way we're changing elements within the class and not adding styles to the actual html element itself. 
+  Makes for less buggy / more cleaner coder. 
+*/ 
